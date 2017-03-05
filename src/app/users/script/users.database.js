b5de9f25f4d76360b5b812b80374952951bea653
Robert <robert-w@Roberts-MacBook-Pro.local>
2017-03-05 17:51:30 -0500

    Started adding a homepage Fixed issues with mocha and users scripts

---
+++ src/app/users/script/users.database.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const path = require('path');
+const logger = require(path.resolve('./config/lib/winston'));
+const config = require(path.resolve('./config/config'));
+const User = mongoose.model('User');
+
+/**
+* @function dropCollection
+* @summary Drops the entire user collection
+* @name exports.dropCollection
+* @static
+* @return {Promise}
+*/
+exports.dropCollection = () => new Promise((resolve, reject) => {
+  logger.info('Dropping User collection');
+  mongoose.connection.db.dropCollection('User', err => {
+    if (err) {
+      logger.error('Unable to drop User collection', err);
+      reject(err);
+    }
+    resolve();
+  });
+});
+
+/**
+* @function populateCollection
+* @summary Populates the user collection with an Admin account
+* @name exports.populateCollection
+* @static
+* @return {Promise}
+*/
+exports.populateCollection = () => new Promise((resolve, reject) => {
+  logger.info('Populating User collection');
+  const {admin} = config.mongo;
+  // Check for an admin user
+  if (!admin || !admin.username || !admin.password) {
+    logger.error('Missing default admin User');
+    return reject();
+  }
+  // Create an admin user if one does not already exist
+  User.findOne({ username: admin.username }).exec().then(user => {
+    if (user) {
+      logger.info('Admin user already created');
+      return resolve();
+    }
+
+    const Administrator = new User({
+      username: admin.username,
+      password: admin.password,
+      firstName: 'Mr.',
+      lastName: 'Admin',
+      email: 'admin@gmail.com'
+    });
+
+    Administrator.save(err => {
+      if (err) {
+        logger.error('Unable to create admin User', err);
+        return reject(err);
+      }
+      logger.info('Successfully created admin user');
+      resolve();
+    });
+
+  });
+
+});
--- src/config/scripts/mocha.js
+++ src/config/scripts/mocha.js
@@ -4,8 +4,8 @@ process.env.NODE_ENV = 'test';
 const Mocha = require('mocha');
 const path = require('path');
 const mongoose = require(path.resolve('./config/lib/mongoose'));
-const logger = require(path.resolve('./config/lib/logger'));
-const config = require(path.resolve('./config/lib/config'));
+const logger = require(path.resolve('./config/lib/winston'));
+const config = require(path.resolve('./config/config'));
 
 const harness = new Mocha({
   ui: 'bdd',
