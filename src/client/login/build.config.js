d5bd3b65068c055730a52b0a73d74749e8815bfe
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-04-01 17:28:22 -0400

    Enabled server rendering and inline styles for the login page

--- src/client/login/build.config.js
+++ src/client/login/build.config.js
@@ -13,7 +13,9 @@ module.exports = {
     }
   },
   build: {
-    criticalStyle: '',
-    prerender: {}
+    criticalStyle: 'login/css/login.scss',
+    prerender: {
+      'login-component': 'login/js/components/Login.js'
+    }
   }
 };
--- src/server/users/controllers/login.controller.js
+++ src/server/users/controllers/login.controller.js
@@ -1,6 +1,7 @@
+const { renderToString } = require('react-dom/server');
+const { createElement } = require('react');
 const path = require('path');
 const { compiledAssets } = require(path.resolve('./config/config'));
-
 
 /**
 * @function login
@@ -11,8 +12,15 @@ const { compiledAssets } = require(path.resolve('./config/config'));
 * @param {Express.Response} res - Express response object
 */
 exports.login = (req, res) => {
+  // Get the markup from our component
+  const markup = compiledAssets.js['login-component']
+    ? renderToString(createElement(compiledAssets.js['login-component'].default))
+    : '';
+
   res.render('login', {
+    critical: compiledAssets.css['login/css/login.scss'],
     common: compiledAssets.js.common,
-    loginjs: compiledAssets.js.login
+    loginjs: compiledAssets.js.login,
+    markup: markup
   });
 };
