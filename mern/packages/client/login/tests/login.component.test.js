33fbf9586eb6b3931955c7dbc0418ca28ef44112
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-04-09 23:31:31 -0400

    Fixed comments and text in login.component.test.js

--- mern/packages/client/login/tests/login.component.test.js
+++ mern/packages/client/login/tests/login.component.test.js
@@ -3,39 +3,39 @@ import { shallow, mount } from 'enzyme';
 import React from 'react';
 
 /**
-* Set up a mock window.location property since login redirects after a successful login
-*/
-Object.defineProperty(window.location, 'href', { writable: true, value: '' });
-
-/**
 * Import with as so that later we can override the default export to mock out it's functionality
 */
 import * as request from 'common/utils/request';
 
+/**
+* Set up a mock window.location property since login redirects after a successful login
+*/
+Object.defineProperty(window.location, 'href', { writable: true, value: '' });
+
 
 describe('Login Component Test', () => {
 
-  test('Rendering the Home component should not throw', () => {
+  test('Rendering the Login component should not throw', () => {
     shallow(<Login />);
   });
 
-  test('Should show the modal if Signup is clicked', () => {
+  test('Should show the modal if Sign Up button is clicked', () => {
     const component = shallow(<Login />);
     expect(component.state('dialogOpen')).toBeFalsy();
     component.find({ label: 'Sign Up' }).simulate('touchTap');
     expect(component.state('dialogOpen')).toBeTruthy();
   });
 
-  test('Should close the modal when shouldCloseDialog is called', () => {
+  test('Should close the modal when closeDialog is called', () => {
     const component = shallow(<Login />);
     component.find({ label: 'Sign Up' }).simulate('touchTap');
     expect(component.state('dialogOpen')).toBeTruthy();
-    // I won't be able to find the RaisedButton inside the dialog, so let's trigger it's callback
+    // I can't seem to find the RaisedButton inside the dialog, so let's trigger it's callback manually
     component.instance().closeDialog();
     expect(component.state('dialogOpen')).toBeFalsy();
   });
 
-  test('Should handle a failed request without an error', async () => {
+  test('Should handle a request error and show the message in the dialog', async () => {
     const component = mount(<Login />);
     const error = new Error('Bad Request');
     // Mock out the request module to reject the request
@@ -45,19 +45,19 @@ describe('Login Component Test', () => {
     expect(component.state('dialogMessage')).toMatch('Bad Request');
   });
 
-  test('Should handle a failed login attempt', async () => {
+  test('Should handle a failed login attempt and show the correct message in the dialog', async () => {
     const component = mount(<Login />);
     const error = new Error('Invalid username/password');
-    // Mock out the request to be resolve incorrectly
+    // Mock out the request to resolve with an error
     request.default = () => new Promise((resolve, _) => resolve(error));
     await component.instance().signIn();
     expect(component.state('dialogOpen')).toBeTruthy();
     expect(component.state('dialogMessage')).toMatch('Invalid username/password');
   });
 
-  test('Should handle a successful login attempt', async () => {
+  test('Should redirect after a successful login attempt', async () => {
     const component = mount(<Login />);
-    // Mock out the request to be resolve with success
+    // Mock out the request to resolve with a status of success
     request.default = () => new Promise((resolve, _) => resolve({ status: 'success' }));
     await component.instance().signIn();
     expect(component.state('dialogOpen')).toBeFalsy();
