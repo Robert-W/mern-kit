3918c786d04cef022de835969ce06c2d67fd01ec
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-05-04 17:35:54 -0400

    Adding some updates to the homepage

--- mern/packages/client/home/components/Home.js
+++ mern/packages/client/home/components/Home.js
@@ -8,10 +8,21 @@ const pageOptions = {
 };
 
 export default class Home extends Component {
+
+  getWelcomeMessage () {
+    // User is a sanitized global from express
+    return app.user
+      ? `Welcome ${app.user.firstName} ${app.user.lastName}!`
+      : 'Who are you?';
+  }
+
   render () {
+    const welcomeMessage = this.getWelcomeMessage();
+
+
     return (
       <Page links={pageOptions.links}>
-
+        <h2 className='home__welcome-banner'>{welcomeMessage}</h2>
       </Page>
     );
   }
