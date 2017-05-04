88a2c26f4166d737518ffc22476e3723b042c5e4
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-05-03 23:40:59 -0400

    Updated header component to use menu

--- mern/packages/client/common/components/Header.js
+++ mern/packages/client/common/components/Header.js
@@ -1,47 +1,34 @@
-import FlatButton from 'material-ui/FlatButton';
+import MoreIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 import AppBar from 'material-ui/AppBar';
 import React, {Component} from 'react';
 
-/* <header className='app__header'>
-  <div className='inner'>
-    <nav className='app__navigation'>
-      <ul className='app__navigation-list'>
-        <li className='app__navigation-list__item'>
-          <FlatButton
-            label='Logout'
-            href='/auth/signout'
-            labelStyle={labelStyle} />
-        </li>
-      </ul>
-    </nav>
-  </div>
-</header> */
+const Menu = (props) => {
+  const iconButton = <IconButton><MoreIcon color='#FFF' /></IconButton>;
+  const { links = {}} = props;
+
+  return (
+    <IconMenu
+      iconButtonElement={iconButton}
+      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}>
+      {links.logout ? <MenuItem primaryText='Logout' title='Logout' href='/auth/signout' /> : null}
+    </IconMenu>
+  );
+};
 
 export default class Header extends Component {
-
   render () {
-    const labelStyle = { textDecoration: 'underline', color: 'white' };
-    const { links = {} } = this.props;
+    const {links} = this.props;
 
     return (
       <AppBar
         title='Mern-Kit'
         className='app__header'
-        showMenuIconButton={false}>
-        <nav className='app__navigation'>
-          <ul className='app__navigation-list'>
-            { !links.logout ? undefined :
-              <li className='app__navigation-list__item'>
-                <FlatButton
-                  label='Logout'
-                  title='Logout'
-                  href='/auth/signout'
-                  labelStyle={labelStyle} />
-              </li>
-            }
-          </ul>
-        </nav>
-      </AppBar>
+        showMenuIconButton={false}
+        iconElementRight={!links ? null : <Menu {...this.props} />} />
     );
   }
 }
