c119f6e727adad4cfbc123a21fb3683af4402f80
Robert Winterbottom <rwinterbottom@asymmetrik.com>
2017-05-05 00:58:07 -0400

    Fixed test for home page and started adding more tests for the home page

--- mern/packages/client/home/tests/home.component.test.js
+++ mern/packages/client/home/tests/home.component.test.js
@@ -8,4 +8,9 @@ describe('Home Component Test', () => {
     shallow(<Home />);
   });
 
+  test('Rendering the Home component with a user should render a welcome message', () => {
+    const defaultUser = { firstName: 'Mr.', lastName: 'Johnson' };
+    shallow(<Home user={defaultUser} />);
+  });
+
 });
